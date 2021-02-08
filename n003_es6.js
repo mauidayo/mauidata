80f9ad9ebd489a558aa4300bb7bccf3cd999eaac
mauidayo <mauidayo@hotmail.com>
2021-02-08 17:16:46 +0900

    2021 02 08 17 16

---
+++ n003_es6.js
@@ -0,0 +1,12 @@
+//es5 스타일
+function printHelloEs5(){
+    console.log('hello es5');
+}
+
+const printHelloEx6 = () => {
+    console.log('hello es6');
+
+}
+
+printHelloEs5();
+printHelloEx6();
---
+++ n004_console.js
@@ -0,0 +1,3 @@
+console.log('하나');
+console.error('하나', '둘');
+console.warn(`여기는 ${'싱클쿼드로'}`);
